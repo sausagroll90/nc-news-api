6aa7db032fc182f6ef335ad5e9058318f8da7552
Dominic Francis <dominicf09@gmail.com>
2024-04-16 15:43:24 +0100

    Add DELETE method to /api/comments/:comment_id endpoint
    Add endpoint which responds with 204 when delete successful, 400 when invalid url, and 404 when given comment doesn't exist.
    Update endpoints.json.
    Add integration tests for happy path and all error cases.

--- model/comments.model.js
+++ model/comments.model.js
@@ -10,7 +10,7 @@ exports.selectCommentsByArticleId = async (id) => {
     [id]
   );
   if (rows.length === 0) {
-    const articleExists = await checkArticleExists(id)
+    const articleExists = await checkArticleExists(id);
     if (!articleExists) {
       return Promise.reject({ status: 404, msg: "article not found" });
     }
@@ -42,4 +42,14 @@ exports.insertComment = async (requestBody, article_id) => {
     throw err;
   }
   return rows[0];
+};
+
+exports.removeComment = async (id) => {
+  const { rows } = await db.query(
+    "DELETE FROM comments WHERE comment_id=$1 RETURNING *",
+    [id]
+  );
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "comment not found" });
+  }
 };
